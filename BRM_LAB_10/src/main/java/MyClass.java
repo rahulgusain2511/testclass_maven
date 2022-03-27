a915f1dae2242268cfa0da94c108abe060f71a9d
rahulgusain2511 <78257755+rahulgusain2511@users.noreply.github.com>
2022-03-27 08:10:55 +0530

    Mycommit2

--- BRM_LAB_10/src/main/java/MyClass.java
+++ BRM_LAB_10/src/main/java/MyClass.java
@@ -2,7 +2,7 @@
 public class MyClass {
 
 	public static void main(String[] args) {
-		System.out.println("We are doing GitHub integration in our Maven project.");
+		System.out.println("We are doing GitHub integration.");
 
 	}
 
